ef2b8cbc6f7afd2257f62a2f1ac4271fa8a32afa
Udriste Andrei <andrei.udriste@gmail.com>
2025-04-24 11:23:25 +0300

    Initial commit: Adăugat proiectul Spring Boot sportbet

---
+++ src/main/java/proiect/bet/sportbet/SportbetApplication.java
@@ -0,0 +1,13 @@
+package proiect.bet.sportbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportbetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportbetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proiect/bet/sportbet/SportbetApplicationTests.java
@@ -0,0 +1,13 @@
+package proiect.bet.sportbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportbetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
